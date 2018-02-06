71a5e28a4dd2f8d89e2c303999e17495b507a9be
lumosnysm <lumosnysm@gmail.com>
2018-02-06 11:02:53 +0700

    tao ban co va nuoc di

--- cell.js
+++ cell.js
@@ -1,3 +1,109 @@
 class Cell {
-    
+    constructor(i, j, w) {
+        this.i = i;
+        this.j = j;
+        this.x = i * w;
+        this.y = j * w;
+        this.w = w;
+        this.hasPiece = false;
+        this.pieceColor = false;
+        this.bg = false;
+        this.possibleDestination = false;
+        this.plane = false;
+        this.castle = false;
+    }
+    show() {
+        stroke(0);
+        if (this.bg) {
+            fill(255, 0, 0);
+        } else if (this.possibleDestination) {
+            fill(100, 255, 100);
+        } else if ((this.i + this.j) % 2 == 0) {
+            fill(200);
+        } else {
+            noFill();
+        }
+        rect(this.x, this.y, this.w, this.w);
+        if (this.castle) {
+            textAlign(CENTER);
+            fill(0);
+            textSize(40);
+            text("T", this.x+this.w*0.5, this.y-15+this.w);
+        }
+        if (this.plane) {
+            textAlign(CENTER);
+            fill(0);
+            textSize(30);
+            text("MB", this.x+this.w*0.5, this.y-15+this.w);
+        }
+        if (this.hasPiece) {
+            if (this.pieceColor) {
+                stroke(0);
+                fill(255);
+                ellipse(this.x + this.w * 0.5, this.y + this.w * 0.5, this.w * 0.5);
+            } else {
+                stroke(0);
+                fill(127);
+                ellipse(this.x + this.w * 0.5, this.y + this.w * 0.5, this.w * 0.5);
+            }
+        }
+    }
+
+    possibleMoves(grid) {
+        let moves = new Array();
+        if (this.hasPiece) {
+            let posX = [this.i + 1, this.i + 2, this.i + 2, this.i + 1, this.i - 1, this.i - 2, this.i - 2, this.i - 1];
+            let posY = [this.j - 2, this.j - 1, this.j + 1, this.j + 2, this.j + 2, this.j + 1, this.j - 1, this.j - 2];
+            let defX = [this.i, this.i + 1, this.i + 1, this.i, this.i, this.i - 1, this.i - 1, this.i];
+            let defY = [this.j - 1, this.j, this.j, this.j + 1, this.j + 1, this.j, this.j, this.j - 1];
+            for (let k = 0; k < 8; k++) {
+                if (posX[k] >= 0 && posX[k] < 8 && posY[k] >= 0 && posY[k] < 8) {
+                    if (!grid[posX[k]][posY[k]].hasPiece || (grid[posX[k]][posY[k]].hasPiece && (grid[posX[k]][posY[k]].pieceColor != this.pieceColor))) {
+                        if (!grid[defX[k]][defY[k]].hasPiece) {
+                            moves.push(grid[posX[k]][posY[k]]);
+                        }
+                    }
+                }
+            }
+        }
+        return moves;
+    }
+
+    isPossibleMove(des) {
+        let moves = this.possibleMoves(grid);
+        for (let i = 0; i < moves.length; i++) {
+            if (des == moves[i]) return true;
+        }
+        return false;
+    }
+
+    clicked() {
+        if (this.hasPiece) {
+            let moves = this.possibleMoves(grid);
+            if (this.bg) {
+                this.bg = false;
+                for (let i = 0; i < moves.length; i++) {
+                    moves[i].possibleDestination = false;
+                }
+            } else {
+                this.bg = true;
+                for (let i = 0; i < moves.length; i++) {
+                    moves[i].possibleDestination = true;
+                }
+            }
+        }
+    }
+
+    playerMove(des) {
+        des.hasPiece = true;
+        if (this.pieceColor) {
+            des.pieceColor = true;
+        } else des.pieceColor = false;
+        this.hasPiece = false;
+        des.clicked();
+    }
+
+    contains(x, y) {
+        return (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.w);
+    }
 }
--- sketch.js
+++ sketch.js
@@ -9,22 +9,66 @@ function make2DArray(cols, rows) {
 const cols = 8;
 const rows = 8;
 const w = 50;
+let previous;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	createCanvas(600, 401);
+	createCanvas(401, 401);
 	grid = make2DArray(cols, rows);
 	for (let i = 0; i < cols; i++) {
 		for (let j = 0; j < rows; j++) {
 			grid[i][j] = new Cell(i, j, w);
 		}
 	}
+	setupGame();
 }
 
 function draw() {
+	background(255);
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++) {
+			grid[i][j].show();
+		}
+	}
+}
 
+function mousePressed() {
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++) {
+			if (grid[i][j].contains(mouseX, mouseY)) {
+				if (previous == undefined) {
+					previous = grid[i][j];
+				} else {
+					if (previous == grid[i][j]) {
+						previous = undefined;
+					} else if (previous.isPossibleMove(grid[i][j])) {
+						previous.clicked();
+						previous.playerMove(grid[i][j]);
+						//console.log('bpoint: ' + bpoint + ' wpoint: ' + wpoint);
+						previous = undefined;
+					} else {
+						previous.clicked();
+						previous = grid[i][j];
+					}
+				}
+				grid[i][j].clicked();
+			}
+		}
+	}
 }
 
 function setupGame() {
-	
+	grid[0][0].hasPiece = true;
+	grid[4][0].hasPiece = true;
+	grid[7][0].hasPiece = true;
+	grid[0][7].hasPiece = true;
+	grid[3][7].hasPiece = true;
+	grid[7][7].hasPiece = true;
+	grid[0][7].pieceColor = true;
+	grid[3][7].pieceColor = true;
+	grid[7][7].pieceColor = true;
+	grid[4][0].castle = true;
+	grid[3][7].castle = true;
+	grid[3][0].plane = true;
+	grid[4][7].plane = true;
 }
